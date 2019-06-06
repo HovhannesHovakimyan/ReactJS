5abd44e93dc27e46af27246b7d535139717d9230
Hovhannes Hovakimyan <hovhannes.hovakimyan@gmail.com>
2019-06-07 03:09:44 +0400

    simple hardcoded app ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+          lodaing: false,
+          character: {}
+        }
+    }
+
+    componentDidMount() {
+      this.setState({
+        lodaing: true
+      })
+      fetch("https://swapi.co/api/people/1")
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            character: data,
+            lodaing: false
+          })
+        })
+    }
+    
+    render() {
+        const text = this.state.lodaing ? "Loading..." : this.state.character.name
+        return (
+            <div>
+                {text}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
